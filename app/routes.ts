994c4b418e7add377b8a1c07447f956d00c3abe1
V-Kish <vasya.kish94@gmail.com>
2025-11-11 18:57:46 +0100

    update v4

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { route, index } from "@react-router/dev/routes";
 
 export default [
     index("routes/login.tsx"),
-    route("/", "routes/ProtectedRoute.tsx", [
-        route("/home", "routes/home.tsx"),
+    route("", "routes/ProtectedRoute.tsx", [
+        route("home", "routes/home.tsx")
     ]),
 ];
